be4b32527b4562bc81bae7332155f87858e7a34e
Jahnavi Patel <j.patel717@mybvc.ca>
2024-11-26 07:01:23 +0530

    Done

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private float gameTimer = 10;
-    
+
     private int _score = 0;
     private float _timer = 0;
-    
+
     public int Score => _score;
     public float Timer => _timer;
-    
+
     public event Action<int> OnScoreChanged;
     public event Action<float> OnTimerChanged;
 
-    
+
     public void Game()
     {
         _score++;
@@ -26,19 +26,19 @@ public class GameManager : MonoBehaviour
         _score = 0;
         _timer = gameTimer;
     }
-    
+
     public void UpdateTimer()
     {
-        _timer-= Time.deltaTime;
+        _timer -= Time.deltaTime;
         OnTimerChanged?.Invoke(_timer);
     }
-    
+
     public void SetHighScore()
     {
         if (_score > GetHighScore())
             PlayerPrefs.SetInt("HighScore", _score);
     }
-    
+
     public int GetHighScore()
     {
         return PlayerPrefs.GetInt("HighScore", 0);
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,13 +8,13 @@ public class UIManager : MonoBehaviour
     [Header("Menu")]
     [SerializeField] private GameObject menuPage;
     [SerializeField] private TMP_Text txtHighScore;
-    
+
     [Header("Game")]
     [SerializeField] private GameObject gamePage;
     [SerializeField] private TMP_Text txtScore;
     [SerializeField] private TMP_Text txtTimer;
     [SerializeField] private Image imgTimer;
-    
+
     [Header("Game Over")]
     [SerializeField] private GameObject gameOverPage;
     [SerializeField] private TMP_Text txtFinalScore;
@@ -25,37 +25,37 @@ public class UIManager : MonoBehaviour
         gamePage.SetActive(false);
         gameOverPage.SetActive(false);
     }
-    
+
     public void ShowGame()
     {
         menuPage.SetActive(false);
         gamePage.SetActive(true);
         gameOverPage.SetActive(false);
     }
-    
+
     public void ShowGameOver(int score)
     {
         menuPage.SetActive(false);
         gamePage.SetActive(false);
         gameOverPage.SetActive(true);
     }
-    
+
     public void SetMenuHighScore(int score)
     {
         txtHighScore.text = "High Score: " + score.ToString();
     }
-    
+
     public void SetGameScore(int score)
     {
         txtScore.text = score.ToString();
     }
-    
+
     public void SetGameTimer(float timer)
     {
         txtTimer.text = "Time: " + timer.ToString("0.00");
         imgTimer.fillAmount = timer / 10;
     }
-    
+
     public void SetGameEndScore(int score, int highScore)
     {
         txtFinalScore.text = "Your Score: " + score.ToString();
@@ -66,3 +66,5 @@ public class UIManager : MonoBehaviour
         }
     }
 }
+
+
